0c2640d67d4c61e6c8aaeaedfe9766b6290e816c
Richard Murphy <richinatl@gmail.com>
2022-05-09 14:35:34 -0400

    still working on ids populating

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,7 +18,7 @@ const userController = {
       .populate({ path: "thoughts", select: "-__v" })
       .populate({ path: "friends", select: "-__v" })
       .select("-__v")
-      .sort({ _id: -1 })
+      // .sort({ _id: -1 })
       .then((dbUserData) => res.json(dbUserData))
       .catch((err) => {
         console.log(err);
